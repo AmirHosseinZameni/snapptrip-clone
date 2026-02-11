6eb29a7174664427e4db80d2ffa0d40e9779da42
AmirHosseinZameni <amirhossein.zameni@gmail.com>
2026-02-11 14:46:03 +0330

    feat(ui): add responsive breadcrumb section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import BreadCrumbs from "./components/BreadCrumbs";
 import Header from "./components/header/Header";
 import HeroSection from "./components/HeroSection";
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <Header />
       <HeroSection />
+      <BreadCrumbs/>
     </>
   );
 }
---
+++ src/components/BreadCrumbs.jsx
@@ -0,0 +1,16 @@
+export default function BreadCrumbs() {
+  return (
+    <div className="pt-6 pb-6 pr-6 pl-6 sm:pr-20.75 sm:pl-20.75">
+      <div
+        className="h-10  bg-[#F8F9FD] rounded-lg pt-2 pr-3 pb-2 pl-3 flex gap-1 items-center text-[12px] font-medium 
+        sm:w-300
+        sm:max-w-300
+        "
+      >
+        <span>بلیط پرواز</span>
+        <span>/</span>
+        <span>بلیط به x</span>
+      </div>
+    </div>
+  );
+}
