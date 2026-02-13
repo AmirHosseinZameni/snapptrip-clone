7d0d77f47bdf03be29d409d78bdd84d9f8762f1e
AmirHosseinZameni <amirhossein.zameni@gmail.com>
2026-02-13 18:06:01 +0330

    feat: add functionality to Rules section

--- src/components/RulesSection.jsx
+++ src/components/RulesSection.jsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import Button from "./Button";
 import DropIcon from "./DropIcon";
 import Table from "./Table";
+
 export default function RulesSection() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleSetOpen() {
+    setIsOpen((curr) => !curr);
+  }
+  
   return (
     <div className="mt-12 xl:max-w-300 xl:mx-auto ">
       <div className="mx-6 flex flex-col gap-2">
-        <h4 className="font-medium text-[16px]">
+        <h4 className="font-medium text-base">
           قوانین حمل بار ایرلاین های تهران لندن
         </h4>
-        <div className="font-regular text-[14px] ">
+        <div className="font-regular text-sm ">
           <p className="text-[#040A1F99]">
             اگر قصد سفر از تهران به لندن را دارید، آشنایی با قوانین حمل بار
             ایرلاین‌های مختلف یک ضرورت مهم پیش از خرید بلیط است. بسته به ایرلاین
@@ -26,23 +34,34 @@ export default function RulesSection() {
           </p>
         </div>
       </div>
-      <div className="overflow-x-auto scrollbar-hide">
-        <Table />
-      </div>
-      <div className="mx-6 mt-6 flex flex-col gap-2">
-        <h4 className="font-medium text-[16px]">Turkish Airlines</h4>
-        <div className="font-regular text-[14px] ">
-          <p className="text-[#040A1F99]">
-            با یک توقف در استانبول، این ایرلاین ترکیه‌ای یکی از پرطرفدارترین
-            گزینه‌ها برای سفر از تهران به لندن است. Turkish Airlines پروازهای
-            روزانه با اتصالات مناسب به فرودگاه‌های هیترو و گتویک لندن ارائه
-            می‌دهد. مزایا: کیفیت خدمات بالا، تنوع ساعت‌های پرواز، امکان توقف
-            کوتاه در استانبول.
-          </p>
+      {isOpen ? (
+        <div className="overflow-x-auto scrollbar-hide">
+          <Table />
         </div>
-        <Button className="flex items-center text-[#575EFF] font-medium text-[14px]">
-          <span>مشاهده کمتر</span>
-          <DropIcon color="#575EFF" style="rotate-180" />
+      ) : (
+        ""
+      )}
+      <div className="mx-6 mt-6 flex flex-col gap-2">
+        { isOpen ? 
+          <>
+            <h4 className="font-medium text-base">Turkish Airlines</h4>
+            <div className="font-regular text-sm ">
+              <p className="text-[#040A1F99]">
+                با یک توقف در استانبول، این ایرلاین ترکیه‌ای یکی از پرطرفدارترین
+                گزینه‌ها برای سفر از تهران به لندن است. Turkish Airlines
+                پروازهای روزانه با اتصالات مناسب به فرودگاه‌های هیترو و گتویک
+                لندن ارائه می‌دهد. مزایا: کیفیت خدمات بالا، تنوع ساعت‌های پرواز،
+                امکان توقف کوتاه در استانبول.
+              </p>
+            </div>
+          </> : ""
+        }
+        <Button
+          className="flex items-center text-[#575EFF] font-medium text-sm cursor-pointer"
+          onClick={handleSetOpen}
+        >
+          <span>{isOpen ? " مشاهده کمتر" : " مشاهده بیشتر"}</span>
+          <DropIcon color="#575EFF" className={isOpen ? "rotate-180" : ""} />
         </Button>
       </div>
     </div>
